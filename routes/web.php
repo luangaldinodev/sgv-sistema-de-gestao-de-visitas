84a77692144c3a656cf73c290e8a2ce0acaf9e95
luangaldinodev <luangaldinodev@gmail.com>
2026-01-26 14:30:16 -0300

    feat: adiciona regras de acesso para tipos de perfis

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\UserRulesMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,7 @@ Route::get('/', function () {
 Route::get('/home', function () {
     return view('home');
 })->middleware(['auth'])->name('home');
+
+Route::get('/adm', function () {
+    return 'Adm';
+})->middleware(UserRulesMiddleware::class);
